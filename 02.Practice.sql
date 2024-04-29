741185e6a37ead882a51319836c67a1f4f0c355d
taewon Kim <ktw1398@gmail.com>
2024-04-29 17:41:53 +0900

    04-29

--- 02.Practice.sql
+++ 02.Practice.sql
@@ -20,13 +20,15 @@ FROM Employees;
 
 --문제4. 부서별로 평균임금, 최고임금, 최저임금을 부서아이디(department_id)와 함께 출력합니다.
 --정렬순서는 부서번호(department_id) 내림차순입니다.
-SELECT department_id 부서번호,
-       ROUND(AVG(salary),1) 평균임금,
+SELECT d.department_id 부서ID,
+       d.department_name 부서명,
+       ROUND(AVG(salary)) 평균임금,
        MAX(salary) 최고임금,
        MIN(salary) 최저임금
-FROM Employees
-GROUP BY department_id
-ORDER BY department_id DESC;
+FROM Employees e
+JOIN Departments d ON e.department_id=d.department_id
+GROUP BY d.department_id, d.department_name
+ORDER BY d.department_id DESC;
 
 --문제5. 업무(job_id)별로 평균임금, 최고임금, 최저임금을 업무아이디(job_id)와 함께 출력하고 정렬
 --순서는 최저임금 내림차순, 평균임금(소수점 반올림), 오름차순 순입니다.
@@ -55,21 +57,23 @@ FROM Employees
 WHERE department_id IS NOT NULL 
 GROUP BY department_id
 HAVING AVG(salary)-MIN(salary) < 2000
-ORDER BY AVG(salary)-MIN(salary) DESC;
+ORDER BY "평균임금 - 최저임금" DESC;
+
+SELECT
+    department_id             부서,
+    AVG(salary)               평균임금,
+    MIN(salary)               최저임금,
+    AVG(salary) - MIN(salary) "평균임금-최저임금"
+FROM employees
+GROUP BY department_id
+HAVING AVG(salary) - MIN(salary) < 2000
+ORDER BY AVG(salary) - MIN(salary) DESC;
 
 --문제8. 업무(JOBS)별로 최고임금과 최저임금의 차이를 출력해보세요.
 --차이를 확인할 수 있도록 내림차순으로 정렬하세요?
 SELECT job_title,
-max_salary,
-min_salary
+max_salary- min_salary
 FROM Jobs;
-
-SELECT d.department_name,
-MAX(e.salary)- MIN(e.salary) "최고임금-최저임금"
-FROM Employees e
-JOIN Departments d ON e.department_id = d.department_id
-GROUP BY d.department_name
-ORDER BY MAX(salary)- MIN(salary) DESC;
 
 --채찍피티 문제01.
 SELECT d.department_name 부서명,
@@ -81,26 +85,51 @@ ORDER BY d.department_name ASC;
 
 --채찍피티 문제02. 사람 이름 더 넣어야
 SELECT d.department_name "부서명",
+e2.first_name || ' ' || e2.last_name 매니저,
 MAX(e.salary) 최고급여
 FROM Employees e
 JOIN Departments d ON e.department_id = d.department_id
-GROUP BY d.department_name;
+JOIN Employees e2 ON e.manager_id = e2.employee_id
+GROUP BY d.department_name, e2.first_name || ' ' || e2.last_name
+ORDER BY 최고급여 DESC;
 
---문제9. 2015년 이후 입사자중 관리자별로 평균급여 최소급여 최대급여를 알아보려고 한다.
+--문제9. 2015년 이후 입사자중 관리자별로 평균급여, 최소급여, 최대급여를 알아보려고 한다.
 --출력은 관리자별로 평균급여가 5000이상 중에 평균급여 최소급여 최대급여를 출력합니다.
 --평균급여의 내림차순으로 정렬하고 평균급여는 소수점 첫째짜리에서 반올림 하여 출력합니다.
 
 SELECT e.manager_id "MNG_ID",
-ROUND(AVG(e.salary)) 평균임금,
-MIN(e.salary) 최저임금,
-MAX(e.salary) 최대임금
-FROM Employees e
-JOIN Employees a ON e.department_id=a.department_id
-GROUP BY e.manager_id
+       e2.first_name 매니저,
+       ROUND(AVG(e.salary)) 평균임금,
+       MIN(e.salary) 최저임금,
+       MAX(e.salary) 최대임금
+    FROM Employees e
+JOIN Employees e2 ON e.manager_id=e2.employee_id
+WHERE e.hire_date >= '2015-01-01'
+GROUP BY e.manager_id, e2.first_name
+HAVING AVG(e.salary) >=5000 
 ORDER BY 평균임금 DESC;
 
+SELECT manager_id,
+    ROUND(AVG(salary), 1),
+    MIN(salary),
+    MAX(salary)
+FROM employees
+WHERE hire_date >= '15/01/01'
+GROUP BY manager_id
+HAVING AVG(salary) >= 5000
+ORDER BY AVG(salary) DESC;
+
 --문제10. 아래회사는 보너스 지급을 위해 직원을 입사일 기준으로 나눌려고 합니다.
---입사일이 02/12/31일 이전이면 '창립맴버, 03년은 '03년입사’, 04년은 ‘04년입사’
+--입사일이 12/12/31일 이전이면 '창립맴버, 13년은 '13년입사’, 14년은 ‘14년입사’
 --이후입사자는 ‘상장이후입사’ optDate 컬럼의 데이터로 출력하세요.
 --정렬은 입사일로 오름차순으로 정렬합니다.
-SELECT 
+SELECT first_name || ' ' last_name,
+hire_date 입사일,
+    CASE
+    WHEN hire_date < '2012-12-31' THEN '창립멤버'
+    WHEN hire_date < '2014-01-01' THEN '13년입사'
+    WHEN hire_date < '2015-01-01' THEN '14년입사'
+        ELSE '상장이후입사'
+        END optDate
+FROM Employees
+ORDER BY hire_date ASC;
--- 03.Practice.sql
+++ 03.Practice.sql
@@ -1,26 +1,56 @@
 --문제1.
 --직원들의 사번(employee_id), 이름(firt_name), 성(last_name)과 부서명(department_name)을
 --조회하여 부서이름(department_name) 오름차순, 사번(employee_id) 내림차순 으로 정렬하세요.(106건)
-SELECT employee_id 사번,
-first_name 이름, 
-last_name 성, 
-department_name 부서명
+SELECT e.employee_id 사번,
+e.first_name 이름, 
+e.last_name 성, 
+d.department_name 부서명
 FROM Employees e
 JOIN Departments d ON e.department_id=d.department_id
+ORDER BY employee_id DESC;
+-- ANSI
+-- JOIN의 의도를 명확하게 하고, 조인 조건과 SELCTION 조건을 분리하는 효과
+-- SIMPLE JOIN, 그냥 둘다 불러오고, 같은 것을 WHERE 조건으로 걸기
+SELECT e.employee_id 사번,
+e.first_name 이름,
+e.last_name 성,
+d.department_name 부서명
+FROM Employees e, Departments d
+WHERE e.department_id=d.department_id
 ORDER BY employee_id DESC;
 
 --문제2.employees 테이블의 job_id는 현재의 업무아이디를 가지고 있습니다.
 --직원들의 사번(employee_id), 이름(firt_name), 급여(salary), 부서명(department_name), 현
 --재업무(job_title)를 사번(employee_id) 오름차순 으로 정렬하세요.
 --부서가 없는 Kimberely(사번 178)은 표시하지 않습니다.(106건)
-SELECT employee_id, first_name, salary, department_name, job_title
+SELECT e.employee_id 사번, 
+e.first_name 이름, 
+e.salary 급여,
+d.department_name 부서명,
+j.job_title "현재 업무"
 FROM Employees e
 JOIN Departments d ON e.department_id = d.department_id
-JOIN JOBS j ON e.job_id=j.job_id
+JOIN Jobs j ON e.job_id=j.job_id
 ORDER BY employee_id ASC;
 
+--SIMPLE JOIN
+SELECT e.employee_id 사번,
+e.first_name 이름,
+e.salary 급여,
+d.department_name 부서명,
+j.job_title "현재 업무"
+FROM Employees e, Departments d, Jobs j
+WHERE e.department_id = d.department_id 
+AND e.job_id = j.job_id
+ORDER BY employee_id ASC;
+
+
 --문제2-1.문제2에서 부서가 없는 Kimberely(사번 178)까지 표시해 보세요(107건)
-SELECT employee_id, first_name, salary, department_name, job_title
+SELECT employee_id 부서ID, 
+first_name 이름, 
+salary 급여, 
+department_name 부서명, 
+job_title "현재 업무"
 FROM Employees e
 JOIN Departments d ON e.department_id = d.department_id(+)
 JOIN JOBS j ON e.job_id=j.job_id
@@ -29,7 +59,10 @@ ORDER BY employee_id ASC;
 --문제3.도시별로 위치한 부서들을 파악하려고 합니다.
 --도시아이디(location_id), 도시명(city), 부서명, 부서아이디를, 도시아이디(오름차순)로 정렬하여 출력하세요
 --부서가 없는 도시는 표시하지 않습니다.(27건)
-SELECT l.location_id, city, department_name, department_id
+SELECT l.location_id 도시ID, 
+city 도시명, 
+department_name 부서명, 
+department_id 부서ID
 FROM Departments d 
 JOIN Locations l ON d.location_id=l.location_id
 ORDER BY city ASC;
@@ -37,7 +70,7 @@ ORDER BY city ASC;
 --문제3-1.문제3에서 부서가 없는 도시도 표시합니다.(43건)
 SELECT l.location_id, city, department_name, department_id
 FROM Departments d 
-JOIN Locations l ON d.location_id(+)=l.location_id
+RIGHT OUTER JOIN Locations l ON d.location_id=l.location_id
 ORDER BY city ASC;
 
 --문제4.지역(regions)에 속한 나라들을 지역이름(region_name), 나라이름(country_name)으로 출력하되 
@@ -63,12 +96,12 @@ WHERE e.hire_date < e2.hire_date;
 --나라명(오름차순)로 정렬하여 출력하세요.
 --값이 없는 경우 표시하지 않습니다.(27건) **
 SELECT  
-       c.country_name, 
-       c.country_id, 
-       l.city, 
-       l.location_id, 
-       d.department_name, 
-       d.department_id
+       c.country_name 나라명, 
+       c.country_id 나라ID, 
+       l.city 도시명, 
+       l.location_id 도시ID, 
+       d.department_name 부서명, 
+       d.department_id 부서ID
 FROM Locations l
 JOIN Countries c ON l.country_id=c.country_id
 JOIN Departments d ON l.location_id = d.location_id
@@ -77,11 +110,11 @@ ORDER BY c.country_name ASC;
 --문제7.job_history 테이블은 과거의 담당업무의 데이터를 가지고 있다.
 --과거의 업무아이디(job_id)가 ‘AC_ACCOUNT’로 근무한 사원의 사번, 이름(풀네임), 업무아이디, 시작일, 종료일을 출력하세요.
 --이름은 first_name과 last_name을 합쳐 출력합니다.(2건)
-SELECT jh.employee_id, 
-first_name || ' ' || last_name "이름(풀네임)",
+SELECT e.employee_id 사번, 
+e.first_name || ' ' || e.last_name "이름(풀네임)",
 jh.job_id 업무ID, 
-start_date 시작일, 
-end_date 종료일
+jh.start_date 시작일, 
+jh.end_date 종료일
 FROM Employees e
 JOIN Job_history jh ON e.employee_id = jh.employee_id
 WHERE jh.job_id = 'AC_ACCOUNT';
@@ -91,12 +124,12 @@ WHERE jh.job_id = 'AC_ACCOUNT';
 --위치(Locations)한 도시(city), 나라(countries)의 이름(countries_name) 
 --그리고 지역구분(Regions)의 이름(resion_name)까지 
 --전부 출력해 보세요.(11건)
-SELECT d.department_id,
-       d.department_name,
-       e.first_name,
-       l.city,
-       c.country_name,
-       r.region_name
+SELECT d.department_id 부서ID,
+       d.department_name 부서명,
+       e.first_name 매니저명,
+       l.city 도시,
+       c.country_name 나라이름,
+       r.region_name 지역이름
     FROM Departments d
     JOIN Employees e ON d.manager_id=e.employee_id
     JOIN Locations l ON d.location_id=l.location_id
